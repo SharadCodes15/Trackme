be1d6f12d1807999f13b3bd0d97ed024a32bd2a9
SharadCodes15 <codebysharad@gmail.com>
2026-01-13 16:58:03 +0530

    secrete key changed

--- TrackMe/app.py
+++ TrackMe/app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 # Configuration
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'dev-key-123')
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 
 db.init_app(app)
 
@@ -484,4 +484,5 @@ with app.app_context():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
 
